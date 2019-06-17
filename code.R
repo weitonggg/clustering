b1e2ccedf97016e2222a0c3a2ec9a95db3f8d719
Wei Tong Toh <runstat@outlook.com>
2019-06-17 22:01:22 +0800

    edit code

--- code.R
+++ code.R
@@ -14,11 +14,15 @@ summary(cust$spend_score)
 hist(cust$annual_income_000)
 hist(cust$spend_score)
 
+# scale data
+scaled_cust <- as.data.frame(scale(cust[,c(4,5)]))
+summary(scaled_cust)
+
 # finding optimal no. of clusters using Elbow method
 kk <- 1:10
 wss <- c()
 for (i in kk){
-  cluster_temp <- kmeans(cust[,c(4,5)], centers = i, nstart = 25)
+  cluster_temp <- kmeans(scaled_cust, centers = i, nstart = 25)
   wss <- c(wss, cluster_temp$tot.withinss)
 }
 plot(wss, type = "b")
@@ -28,7 +32,7 @@ plot(wss, type = "b")
 kk <- 2:10
 avg_silhou <- c()
 for (i in kk){
-  cluster_temp <- kmeans(cust[,c(4,5)], centers = i, nstart = 25)
+  cluster_temp <- kmeans(scaled_cust, centers = i, nstart = 25)
   ss <- silhouette(cluster_temp$cluster, dist(cust[,c(4,5)]))
   avg_silhou <- c(avg_silhou, mean(ss[,3]))
 }
@@ -37,7 +41,7 @@ plot(avg_silhou,type="b")
 
 # finding optimal no. of clusters using gap statistic method
 set.seed(123)
-gap_stat <- clusGap(cust[,c(4,5)], FUN = kmeans, nstart = 25,
+gap_stat <- clusGap(scaled_cust, FUN = kmeans, nstart = 25,
                     K.max = 10, B = 50)
 # print result
 print(gap_stat, method = "firstmax")
@@ -46,11 +50,11 @@ fviz_gap_stat(gap_stat)
 # From plot above, maximum gap statistic occurs at no. of clusters, k = 5.
 
 # we will use k-means cluster with k = 5 to segment the data
-set.seed(10)
-cluster_5 <- kmeans(cust[,c(4,5)], centers = 5, nstart = 25)
+set.seed(100)
+km_clust <- kmeans(scaled_cust, centers = 5, nstart = 25)
 #cluster_5$tot.withinss
-cust <- cust %>% mutate(cluster5 = cluster_5$cluster)
-ggplot(cust, aes(x = annual_income_000, y = spend_score, colour = factor(cluster5))) +
+cust <- cust %>% mutate(km_clusters = km_clust$cluster)
+ggplot(cust, aes(x = annual_income_000, y = spend_score, colour = factor(km_clusters))) +
   geom_point() +
   scale_color_discrete(name = "Clusters") +
   xlab("Annual income of customers (thousands)") +
